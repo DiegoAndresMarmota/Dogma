469d0408e105291a23dad25faf2334566305b37e
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-08-14 14:34:25 -0400

    Se genera species

---
+++ enterobacteriaceae_species.py
@@ -0,0 +1,311 @@
+import enterobacteriaceae_genero as Enterobacteriaceae
+
+
+class Biostraticola (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Buttiauxella (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Cedecea (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Citrobacter (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Dickeya (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Enterobacillus (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Escherichia (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Franconibacter (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Gibbsiella (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Klebsiella (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Kluyvera (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Kosakonia (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Leclercia (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Lelliotia (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Limnobaculum (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Mangrovibacter (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Metakosakonia (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Phytobacter (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Pluralibacater (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Pseudescherichia (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Pseudocitrobacter (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Raoultella (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name):
+
+
+class RibonucleicAcid (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Saccharomyces (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Salmonella (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Staphylococcus (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Serratia (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Shimwellia (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Staphylococcus (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Staphylococcus (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
+
+
+class Yokenella (Enterobacteriaceae):
+    def __init__(self, name, genus, species):
+        super().__init__(name, genus, species)
+        self.name = name
+        self.genus = genus
+        self.species = species
+        ## 
+        return Enterobacteriaceae.Enterobacteriaceae(name)
